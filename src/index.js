26827026038d4c649c83e4bd0c20960bc634ab9d
Your Name <ustinovoleksij@gmail.com>
2022-10-04 13:10:46 +0300

    HLEG-14 <Added a paint li function>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    const li = document.querySelectorAll('.item');
+    li.forEach((item) => item.style.backgroundColor="red");
+}
 
-export const findElement = () => {};
+export const findElement = () => {
+    document.querySelectorAll('.card');
+    document.getElementsByTagName('li');
+    document.getElementsByClassName('.card')
+};
